a63ff5bccf78939184360ff17a9dff202584fa00
mharikasridevi <harikasridevi.maddela@gmail.com>
2026-06-19 14:25:37 +0530

    Create Password_checker.py

---
+++ Password_checker.py
@@ -0,0 +1,22 @@
+password = input("Enter a password: ")
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if any(char.isupper() for char in password):
+    score += 1
+
+if any(char.islower() for char in password):
+    score += 1
+
+if any(char.isdigit() for char in password):
+    score += 1
+
+# Improved scoring logic for submission
+if score <= 2:
+    print("Weak Password")
+elif score == 3:
+    print("Medium Password")
+else:
+    print("Strong Password")
